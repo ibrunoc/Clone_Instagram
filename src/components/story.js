59d07b0401a74dd307467a89153d429bbf3d0615
Bruno Costa C <ihyperbr@gmail.com>
2022-05-02 10:26:54 -0300

    Add files via upload

---
+++ src/components/story.js
@@ -0,0 +1,132 @@
+import perfil from './imagens/perfil.jpg';
+import perfil2 from './imagens/perfil2.jpg';
+import perfil3 from './imagens/perfil3.jpg';
+import perfil4 from './imagens/perfil4.jpg';
+import perfil5 from './imagens/perfil5.jpg';
+import perfil6 from './imagens/perfil6.jpg';
+import perfil7 from './imagens/perfil7.jpg';
+import perfil8 from './imagens/perfil8.jpg';
+
+function Story() {
+  return (
+    <>
+      <div className="story">
+            <div className="story-container">
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name18</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil2} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name2</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil3} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name5</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil4} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name3</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil5} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name32</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil6} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name22</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil7} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name98</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil8} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name0</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name18</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil2} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name2</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil3} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name5</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil4} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name3</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil5} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name32</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil6} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name22</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil7} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name98</h1>
+                </div>
+
+                <div className="perfil-story">
+                    <div className="story-border">
+                        <img src={perfil8} className="foto-story" alt="perfil" />
+                    </div>  
+                    <h1>user.name0</h1>
+                </div>
+            </div>
+      </div>
+    </>
+  );
+}
+
+export default Story; 
